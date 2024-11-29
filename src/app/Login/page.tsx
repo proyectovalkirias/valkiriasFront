b2a39eb72e071136f1a0dc182483427f34358035
camimontechiani <cmontechiani2@gmail.com>
2024-11-29 13:23:59 -0300

    inicio sesion google CASI HECHO, toma para vos rocio gila robada

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 import React from 'react';
 
+const GOOGLE_CLIENT_ID = "634423829747-32kn123g67grqggkm2v14f6agaiiu6hp.apps.googleusercontent.com";
+const REDIRECT_URI = "http://localhost:3001/Landing";
+
 const Login = () => {
-  const handleGoogleLogin = () => {
-    window.location.href = `http://localhost:3000/google/redirect`;
-  };
+    const handleLogin = () => {
+        const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${encodeURIComponent(
+            REDIRECT_URI
+        )}&scope=openid%20email%20profile`;
+        window.location.href = googleAuthUrl; // Redirige al usuario a Google
+    };
 
   return (
     <div>
-      <button onClick={handleGoogleLogin}>Iniciar sesión con Google</button>
+      <button onClick={handleLogin}>Iniciar sesión con Google</button>
     </div>
   );
 };
