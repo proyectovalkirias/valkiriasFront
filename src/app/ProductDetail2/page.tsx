40022474054e532898472fd7cc9ad83bac26c702
Serafin Zanoni <serazanoni@gmail.com>
2024-12-06 16:35:33 -0300

    produtc detail buzo accesorios, link desde el home

--- src/app/ProductDetail2/page.tsx
+++ src/app/ProductDetail2/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
+import Image from "next/image";
+import BuzoOversize from "../../../public/images/BuzoOversize.jpg";
 const ProductDetail: React.FC = () => {
   const [ideas, setIdeas] = useState("");
   const [selectedSize, setSelectedSize] = useState<string>("");
@@ -55,12 +56,12 @@ const ProductDetail: React.FC = () => {
       <div className="flex mx-16 gap-4 h-[600px] w-1/2">
         <div className="flex flex-col gap-4 w-1/2">
           <img
-            src="https://th.bing.com/th/id/R.d73839efa381d261e544ece6dea8c7f4?rik=F3331MdhrEa2SA&pid=ImgRaw&r=0"
+            src="https://th.bing.com/th/id/OIP.3zxlkdT9kHDHDhduW7u8FgAAAA?rs=1&pid=ImgDetMain"
             alt="Remera 1"
             className="object-cover rounded-lg w-full h-1/2"
           />
           <img
-            src="https://th.bing.com/th/id/R.d73839efa381d261e544ece6dea8c7f4?rik=F3331MdhrEa2SA&pid=ImgRaw&r=0"
+            src="https://th.bing.com/th/id/OIP.3zxlkdT9kHDHDhduW7u8FgAAAA?rs=1&pid=ImgDetMain"
             alt="Remera 2"
             className="object-cover rounded-lg w-full h-1/2"
           />
@@ -68,7 +69,7 @@ const ProductDetail: React.FC = () => {
 
         <div className="w-1/2">
           <img
-            src="https://th.bing.com/th/id/R.d73839efa381d261e544ece6dea8c7f4?rik=F3331MdhrEa2SA&pid=ImgRaw&r=0"
+            src="https://th.bing.com/th/id/OIP.3zxlkdT9kHDHDhduW7u8FgAAAA?rs=1&pid=ImgDetMain"
             alt="Remera 3"
             className="object-cover rounded-lg w-full h-full"
           />
@@ -77,7 +78,7 @@ const ProductDetail: React.FC = () => {
 
       <div className="w-1/2 max-w-4xl text-center p-6">
         <h1 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-4">
-          Remera unisex para estampa
+          Accesorios
         </h1>
         <p className="text-lg text-gray-800 mb-6">
           Nosotros seguimos trabajando en cada uno de sus personalizados. ¿Ya
