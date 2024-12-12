a4e6cbfc3da311565686c79a1298bf941fc20973
biagio360 <facundobiagioni@gmail.com>
2024-12-12 02:58:47 -0300

    orders vacio

--- src/app/Dashboard/layout.tsx
+++ src/app/Dashboard/layout.tsx
@@ -4,12 +4,12 @@ import Orders from "./Orders/page";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row h-screen bg-[#b093bf] py-12 md:py-20 justify-center items-center">
-      <div className="md:w-1/3 p-6">
+    <div className="flex flex-col md:flex-row h-screen bg-[#b093bf]">
+      <div className="flex flex-col justify-center items-center  md:w-1/4 h-full p-6 border-r-2 border-gray-300">
         <Profile />
       </div>
-      <div className="h-1 bg-gray-300 md:hidden"></div>
-      <div className="flex justify-center md:w-2/3 p-6">
+
+      <div className="flex justify-center items-center md:w-2/3 p-6">
         <div className="w-full max-w-3xl">
           <Orders />
         </div>
--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -5,33 +5,34 @@ const Profile: React.FC = () => {
   const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
-    // Recuperamos los datos del usuario desde localStorage
     const userData = localStorage.getItem("user");
     if (userData) {
-      setUser(JSON.parse(userData)); // Establecemos los datos del usuario
+      setUser(JSON.parse(userData));
     }
   }, []);
 
-  // Si no hay datos de usuario, mostramos un mensaje
   if (!user) {
     return <div>Usuario no autenticado.</div>;
   }
 
   return (
-    <div className="flex flex-col items-center justify-center mt-16 md:mt-0">
+    <div className="flex flex-col items-center justify-center h-full">
       <h1 className="text-4xl md:text-5xl text-[#e5ded3] mb-6">Mi perfil</h1>
 
-      {/* Imagen con foto de Google o predeterminada */}
       <img
-        src={user.photoUrl || "/images/Homevalkirias.jpg"}  // Foto de Google o predeterminada
+        src={user.photoUrl || "/images/Homevalkirias.jpg"}
         alt="Profile emoji"
         className="rounded-full border-4 border-white w-[140px] md:w-[170px] lg:w-[200px]"
       />
 
       <div className="text-base md:text-xl mt-6">
         <h2 className="font-semibold mb-3">Info personal</h2>
-        <p className="mb-2">Nombre: <span className="font-light">{user.name}</span></p>
-        <p className="mb-2">Email: <span className="font-light">{user.email}</span></p>
+        <p className="mb-2">
+          Nombre: <span className="font-light">{user.name}</span>
+        </p>
+        <p className="mb-2">
+          Email: <span className="font-light">{user.email}</span>
+        </p>
       </div>
     </div>
   );
