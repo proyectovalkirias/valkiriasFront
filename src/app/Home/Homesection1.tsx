63de77984a6551e07b06c66f55bc6c07e10d7fc2
camimontechiani <cmontechiani2@gmail.com>
2024-12-16 10:29:28 -0300

    push completo

--- src/app/Home/Homesection1.tsx
+++ src/app/Home/Homesection1.tsx
@@ -1,29 +1,24 @@
-
 import React from 'react';
 
 const HomeSection1: React.FC = () => {
   return (
-    <>
     <section
-    id='Homesection1'
-    className="h-screen w-screen bg-cover bg-center flex items-center justify-center"
-    style={{ backgroundImage: "url('/images/valkiriashirt.jpg')" }}
+      id="Homesection1"
+      className="h-screen flex-1 bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/images/valkiriashirt.jpg')" }}
     >
-      <div className="p-6 rounded-lg">
+      {/* Logo y texto arriba a la izquierda */}
       <div className="absolute top-10 left-10">
         <img
-          src="/images/valkiriaslogo.jpg" 
+          src="/images/valkiriaslogo.jpg"
           alt="Logo de Valkirias"
-          className="h-20 w-auto object-contain" 
+          className="h-20 w-auto object-contain"
         />
-            <p className="text-lg text-creativity-purple mt-2">
+        <p className="text-lg text-creativity-purple mt-2">
           Tu creatividad en tus prendas
         </p>
       </div>
-
-      </div>
     </section>
-  </>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-// import Navbar from "@/components/Navbar";
 import Home from "./page";
 import Sidebar from "@/components/Sidebar";
 
@@ -30,11 +29,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
+    <body className="flex h-screen">
+      {/* Sidebar */}
+      <Sidebar />
 
-      <body>
-       <Sidebar/>
+      {/* Contenido principal */}
+      <main className="flex-1 overflow-auto">
         {children}
-      </body>
-    </html>
+      </main>
+    </body>
+  </html>
   );
 }
