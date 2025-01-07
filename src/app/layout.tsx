546193d35ee6dcd6c090457832cd484631ca6493
biagio360 <facundobiagioni@gmail.com>
2025-01-06 23:01:09 -0300

    nada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Toaster } from "react-hot-toast"; // Importar Toaster
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import SidebarMini from "@/components/SidebarMini";
