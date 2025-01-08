9afd5f837ec6311aa3ea89f35881f4312d7fc51e
Serafin Zanoni <serazanoni@gmail.com>
2025-01-08 17:28:05 -0300

    actualizacion

--- src/app/Products/[id]/page.tsx
+++ src/app/Products/[id]/page.tsx
@@ -229,9 +229,6 @@ const ProductDetail: React.FC = () => {
           <Image
             src={mainImage}
             alt={product.name}
-            className="h-[500px] w-auto mx-auto rounded-xl shadow-md object-contain"
-            width={500}
-            height={500}
             className="w-[500px] aspect-square mx-auto rounded-xl shadow-md"
             width={100}
             height={100}
