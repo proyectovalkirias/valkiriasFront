dfccad46cc48a99108da3da65c4069945dfbe433
camimontechiani <cmontechiani2@gmail.com>
2025-01-16 12:06:19 -0300

    cambio chtabot

--- src/app/Valkibot/livechat/page.tsx
+++ src/app/Valkibot/livechat/page.tsx
@@ -8,97 +8,55 @@ const LiveChatComponent = () => {
   >([]);
   const [input, setInput] = useState("");
   const [socket, setSocket] = useState<any>(null);
-  const [isAdmin, setIsAdmin] = useState<boolean>(false);
-  const [firstname, setFirstname] = useState<string>("");
-  const [userId, setUserId] = useState<string>("");
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [firstname, setFirstname] = useState<string>("guest");
 
   const messagesEndRef = useRef<HTMLDivElement>(null); // Referencia para hacer scroll al final
 
   const API_URL =
     process.env.REACT_APP_API_URL || "https://valkiriasback.onrender.com";
 
-    const getUserDetails = () => {
-      const storedUser = localStorage.getItem("user");
-      if (storedUser) {
-        try {
-          const parsedUser = JSON.parse(storedUser);
-          return {
-            isAdmin: parsedUser.isAdmin || false,
-            firstname: parsedUser.firstname || "guest",
-            userId: parsedUser.id || "guest",
-          };
-        } catch (error) {
-          return { isAdmin: false, firstname: "guest", userId: "guest" };
-        }
-      }
-      return { isAdmin: false, firstname: "guest", userId: "guest" };
-    };
-    
-
   useEffect(() => {
-    const userDetails = getUserDetails();
-    if (userDetails.userId !== "guest") {
-      setIsLoggedIn(true);
-      setIsAdmin(userDetails.isAdmin);
-      setFirstname(userDetails.firstname);
-      setUserId(userDetails.userId);
+    // No es necesario verificar si el usuario está logueado
+    const socketUrl = `wss://valkiriasback.onrender.com`;
+    const newSocket = io(socketUrl);
 
-      const socketUrl = `wss://valkiriasback.onrender.com/?isAdmin=${
-        userDetails.isAdmin
-      }&firstname=${encodeURIComponent(userDetails.firstname)}&userId=${
-        userDetails.userId
-      }`;
-      const newSocket = io(socketUrl);
+    setSocket(newSocket);
 
-      setSocket(newSocket);
+    const fetchMessages = async () => {
+      try {
+        const response = await fetch(`${API_URL}/valkibot/messages/guest`);
+        const data = await response.json();
+        const messagesArray = data[0]?.messages || [];
+        const mappedMessages = messagesArray.map((message: any) => ({
+          sender: message.sender,
+          content: message.content,
+        }));
 
-      const fetchMessages = async () => {
-        try {
-          const response = await fetch(
-            `${API_URL}/valkibot/messages/${userDetails.userId}`
-          );
-          const data = await response.json();
-          const messagesArray = data[0]?.messages || [];
-          const mappedMessages = messagesArray.map((message: any) => ({
-            sender: message.sender,
-            content: message.content,
-          }));
-
-          setMessages([
-            {
-              sender: "Sistema",
-              content:
-                "¡Te estamos derivando al chat en vivo para que hables con un humano! 💬 Nuestro equipo está listo para ayudarte. 🙌",
-            },
-            ...mappedMessages,
-          ]);
-        } catch (error) {
-          console.error("Error al obtener los mensajes:", error);
-        }
-      };
-
-      fetchMessages();
-
-      newSocket.on("chatToClient", (message) => {
-        setMessages((prev) => [
-          ...prev,
-          { sender: message.sender, content: message.message },
+        setMessages([
+          {
+            sender: "Sistema",
+            content:
+              "¡Te estamos derivando al chat en vivo para que hables con un humano! 💬 Nuestro equipo está listo para ayudarte. 🙌",
+          },
+          ...mappedMessages,
         ]);
-      });
+      } catch (error) {
+        console.error("Error al obtener los mensajes:", error);
+      }
+    };
 
-      return () => {
-        newSocket.disconnect();
-      };
-    } else {
-      setMessages([
-        {
-          sender: "Sistema",
-          content:
-            "Inicia sesión o regístrate para comunicarte con un miembro del equipo.",
-        },
+    fetchMessages();
+
+    newSocket.on("chatToClient", (message) => {
+      setMessages((prev) => [
+        ...prev,
+        { sender: message.sender, content: message.message },
       ]);
-    }
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
   }, []);
 
   const sendMessage = () => {
@@ -123,10 +81,6 @@ const LiveChatComponent = () => {
       messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
-
-  if (isLoggedIn === null) {
-    return <div>...Cargando</div>;
-  }
 
   return (
     <div className="fixed bottom-36 right-5 z-50 flex flex-col w-[350px] h-[500px] bg-[#F3E8FF] rounded-2xl shadow-lg overflow-hidden">
@@ -158,13 +112,11 @@ const LiveChatComponent = () => {
           onChange={(e) => setInput(e.target.value)}
           placeholder="Escribe un mensaje..."
           className="flex-1 p-1 border border-gray-300 rounded-md mr-2 text-black text-sm"
-          disabled={!isLoggedIn}
           onKeyDown={handleKeyDown}
         />
         <button
           onClick={sendMessage}
           className="py-1 px-3 bg-[#3e1a4d] text-white rounded-md text-sm"
-          disabled={!isLoggedIn}
         >
           Enviar
         </button>
